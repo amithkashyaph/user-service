95c20b832602ba9c9f0ba8dbefdf85be606df851
amithkashyap <amithkashyaprv@gmail.com>
2024-06-24 19:19:32 +0530

    added dependent components in the service implementation

--- src/main/java/com/ecommerce/userservice/service/AuthServiceImpl.java
+++ src/main/java/com/ecommerce/userservice/service/AuthServiceImpl.java
@@ -2,12 +2,30 @@ package com.ecommerce.userservice.service;
 
 import com.ecommerce.userservice.dto.UserDto;
 import com.ecommerce.userservice.entity.SessionStatus;
+import com.ecommerce.userservice.entity.User;
+import com.ecommerce.userservice.repository.SessionRepository;
+import com.ecommerce.userservice.repository.UserRespository;
 import com.ecommerce.userservice.service.interfaces.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+
+    private UserRespository userRespository;
+
+    private SessionRepository sessionRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public AuthServiceImpl(UserRespository userRespository, SessionRepository sessionRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRespository = userRespository;
+        this.sessionRepository = sessionRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
     @Override
     public ResponseEntity<UserDto> login(String email, String password) {
         return null;
@@ -19,7 +37,8 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public ResponseEntity<UserDto> signup(String email, String password) {
+    public UserDto signup(String email, String password) {
+        User user = new User();
         return null;
     }
 
