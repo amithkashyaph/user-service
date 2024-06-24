f9054f5e8a14a08296ed7cc48b172ec8d5b10836
amithkashyap <amithkashyaprv@gmail.com>
2024-06-24 17:48:31 +0530

    Added Authservice initial implementation code

--- src/main/java/com/ecommerce/userservice/service/AuthServiceImpl.java
+++ src/main/java/com/ecommerce/userservice/service/AuthServiceImpl.java
@@ -1,4 +1,30 @@
 package com.ecommerce.userservice.service;
 
-public class AuthServiceImpl {
+import com.ecommerce.userservice.dto.UserDto;
+import com.ecommerce.userservice.entity.SessionStatus;
+import com.ecommerce.userservice.service.interfaces.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    @Override
+    public ResponseEntity<UserDto> login(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> logout(String token, Long userId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<UserDto> signup(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public SessionStatus validate(String token, Long userId) {
+        return null;
+    }
 }
