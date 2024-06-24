1c4bb41c9ed5ec65223007ea667e31e8aaed4229
amithkashyap <amithkashyaprv@gmail.com>
2024-06-24 19:15:14 +0530

    Added User entity implementation

--- src/main/java/com/ecommerce/userservice/entity/User.java
+++ src/main/java/com/ecommerce/userservice/entity/User.java
@@ -1,4 +1,23 @@
 package com.ecommerce.userservice.entity;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class User extends BaseModel {
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Role> roles = new HashSet<>();
 }
