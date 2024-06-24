e69a40055bee14ae5036eb1370f1a6cad0b79930
amithkashyap <amithkashyaprv@gmail.com>
2024-06-24 19:10:15 +0530

    Added Auth service interface

--- src/main/java/com/ecommerce/userservice/service/interfaces/AuthService.java
+++ src/main/java/com/ecommerce/userservice/service/interfaces/AuthService.java
@@ -1,4 +1,16 @@
 package com.ecommerce.userservice.service.interfaces;
 
+import com.ecommerce.userservice.dto.UserDto;
+import com.ecommerce.userservice.entity.SessionStatus;
+import org.springframework.http.ResponseEntity;
+
 public interface AuthService {
+
+    ResponseEntity<UserDto> login(String email, String password);
+
+    ResponseEntity<Void> logout(String token, Long userId);
+
+    ResponseEntity<UserDto> signup(String email, String password);
+
+    SessionStatus validate(String token, Long userId);
 }
