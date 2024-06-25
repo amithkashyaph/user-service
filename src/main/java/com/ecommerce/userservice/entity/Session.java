6410e32817968369492d01f56bdf158aae578e01
amithkashyap <amithkashyaprv@gmail.com>
2024-06-25 18:15:17 +0530

    added Session related data fields within entity

--- src/main/java/com/ecommerce/userservice/entity/Session.java
+++ src/main/java/com/ecommerce/userservice/entity/Session.java
@@ -1,11 +1,21 @@
 package com.ecommerce.userservice.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Getter
 @Setter
 @Entity
 public class Session extends BaseModel {
+    private String token;
+    private Date expiringAt;
+
+    @ManyToOne
+    private User user;
+
+    @Enumerated(EnumType.ORDINAL)
+    private SessionStatus sessionStatus;
 }
