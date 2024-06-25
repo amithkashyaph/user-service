3eec1923ae7310d52840592326e6c59d24b0ad41
amithkashyap <amithkashyaprv@gmail.com>
2024-06-25 22:58:59 +0530

    added initial Auth controller logic

--- src/main/java/com/ecommerce/userservice/controller/AuthController.java
+++ src/main/java/com/ecommerce/userservice/controller/AuthController.java
@@ -1,4 +1,17 @@
 package com.ecommerce.userservice.controller;
 
+import com.ecommerce.userservice.dto.SignUpRequestDto;
+import com.ecommerce.userservice.dto.UserDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    public ResponseEntity<UserDto> signup(@RequestBody SignUpRequestDto signUpRequestDto) {
+        return null;
+    }
 }
