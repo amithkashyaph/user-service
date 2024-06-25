7f4cfff5c61d890a7ddd8736c6e23e3cebb9432c
amithkashyap <amithkashyaprv@gmail.com>
2024-06-25 18:12:27 +0530

    annotated initial Session entity

--- src/main/java/com/ecommerce/userservice/entity/Session.java
+++ src/main/java/com/ecommerce/userservice/entity/Session.java
@@ -1,4 +1,11 @@
 package com.ecommerce.userservice.entity;
 
-public class Session {
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Session extends BaseModel {
 }
