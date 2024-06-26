17017b13c8e7b58a38a0d65b4871c2772c0b0966
amithkashyap <amithkashyaprv@gmail.com>
2024-06-26 18:55:23 +0530

    added Jpa repository and annotated Session repository interface

--- src/main/java/com/ecommerce/userservice/repository/SessionRepository.java
+++ src/main/java/com/ecommerce/userservice/repository/SessionRepository.java
@@ -1,4 +1,9 @@
 package com.ecommerce.userservice.repository;
 
-public interface SessionRepository {
+import com.ecommerce.userservice.entity.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
 }
