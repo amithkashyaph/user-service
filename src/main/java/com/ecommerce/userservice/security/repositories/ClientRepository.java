4e246a62b9980cd611065c4635cd682091f32679
amithkashyap <amithkashyaprv@gmail.com>
2024-06-29 09:59:52 +0530

    implemented Client repository

--- src/main/java/com/ecommerce/userservice/security/repositories/ClientRepository.java
+++ src/main/java/com/ecommerce/userservice/security/repositories/ClientRepository.java
@@ -1,4 +1,13 @@
 package com.ecommerce.userservice.security.repositories;
 
-public interface ClientRepository {
+import java.util.Optional;
+
+
+import com.ecommerce.userservice.security.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
 }
