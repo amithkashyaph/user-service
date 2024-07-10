60e6503133c36740edde1095f33b582f9b54e7c5
amithkashyap <amithkashyaprv@gmail.com>
2024-07-11 01:56:36 +0530

    added Logout request dto

--- src/main/java/com/ecommerce/userservice/dto/LogoutRequestDto.java
+++ src/main/java/com/ecommerce/userservice/dto/LogoutRequestDto.java
@@ -1,4 +1,11 @@
 package com.ecommerce.userservice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LogoutRequestDto {
+    private String token;
+    private Long userId;
 }
