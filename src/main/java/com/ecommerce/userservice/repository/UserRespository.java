b419c838a817b63beb3a55dbbfe8ce9ecbc6629f
amithkashyap <amithkashyaprv@gmail.com>
2024-08-03 01:49:50 +0530

    implemented findByEmail api

--- src/main/java/com/ecommerce/userservice/repository/UserRespository.java
+++ src/main/java/com/ecommerce/userservice/repository/UserRespository.java
@@ -1,4 +1,12 @@
 package com.ecommerce.userservice.repository;
 
-public interface UserRespository {
+import com.ecommerce.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRespository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
