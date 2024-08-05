19907f545d081ff8c68f4eb79b94ba88c0a0e0b2
amithkashyap <amithkashyaprv@gmail.com>
2024-08-05 23:38:22 +0530

    implemented find by token and userid api

--- src/main/java/com/ecommerce/userservice/repository/SessionRepository.java
+++ src/main/java/com/ecommerce/userservice/repository/SessionRepository.java
@@ -4,6 +4,9 @@ import com.ecommerce.userservice.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
+    Optional<Session> findByTokenAndUser_Id(String token, Long userId);
 }
