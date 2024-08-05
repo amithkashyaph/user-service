83a01ad7158e84ec5e232930571eabb7404fa8ce
amithkashyap <amithkashyaprv@gmail.com>
2024-08-05 23:37:07 +0530

    implemented user dto class

--- src/main/java/com/ecommerce/userservice/dto/UserDto.java
+++ src/main/java/com/ecommerce/userservice/dto/UserDto.java
@@ -1,4 +1,28 @@
 package com.ecommerce.userservice.dto;
 
+import com.ecommerce.userservice.entity.Role;
+import com.ecommerce.userservice.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
 public class UserDto {
+
+    private String email;
+
+    @JsonIgnore
+    private Set<Role> roles = new HashSet<>();
+
+    public static UserDto from(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setEmail(user.getEmail());
+        userDto.setRoles(user.getRoles());
+
+        return userDto;
+    }
 }
