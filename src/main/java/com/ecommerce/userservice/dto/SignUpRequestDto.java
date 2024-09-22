54afeda030569013f76eb3d907a342f3d2487213
amithkashyap <amithkashyaprv@gmail.com>
2024-09-22 23:37:30 +0530

    added signup request dto

--- src/main/java/com/ecommerce/userservice/dto/SignUpRequestDto.java
+++ src/main/java/com/ecommerce/userservice/dto/SignUpRequestDto.java
@@ -1,4 +1,11 @@
 package com.ecommerce.userservice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignUpRequestDto {
+    private String email;
+    private String password;
 }
