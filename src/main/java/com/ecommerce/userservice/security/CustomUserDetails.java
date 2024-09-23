ea62cdad7ad99551547a14b3a16c8b6c3e304e9a
amithkashyap <amithkashyaprv@gmail.com>
2024-09-23 20:57:44 +0530

    refactored custom user details class

--- src/main/java/com/ecommerce/userservice/security/CustomUserDetails.java
+++ src/main/java/com/ecommerce/userservice/security/CustomUserDetails.java
@@ -1,6 +1,9 @@
 package com.ecommerce.userservice.security;
 
+import com.ecommerce.userservice.entity.Role;
 import com.ecommerce.userservice.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +11,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
+@JsonDeserialize(as = CustomUserDetails.class)
 public class CustomUserDetails implements UserDetails {
     private User user;
 
@@ -19,37 +26,53 @@ public class CustomUserDetails implements UserDetails {
     public CustomUserDetails(User user) {
         this.user = user;
     }
+
+    public CustomUserDetails() {}
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        Set<Role> roles = user.getRoles();
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for(Role role: roles) {
+            grantedAuthorities.add(new CustomeGrantedAuthority(role));
+        }
+
+        return grantedAuthorities;
     }
 
     @Override
+    @JsonIgnore
     public String getPassword() {
         return user.getPassword();
     }
 
     @Override
+    @JsonIgnore
     public String getUsername() {
         return user.getEmail();
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isEnabled() {
         return true;
     }
