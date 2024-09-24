4209176182b42798ebee0a791ae7dbedc7cb68fe
amithkashyap <amithkashyaprv@gmail.com>
2024-09-24 23:29:25 +0530

    added initial test for registered client add

---
+++ src/test/java/com/ecommerce/userservice/RegisteredClientAddTest.java
@@ -0,0 +1,19 @@
+package com.ecommerce.userservice;
+
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
+
+@SpringBootTest
+public class RegisteredClientAddTest {
+}
