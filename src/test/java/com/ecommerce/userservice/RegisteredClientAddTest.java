d463569fa2fa4c6dd15c0115421f15ebe4e1c6eb
amithkashyap <amithkashyaprv@gmail.com>
2024-09-24 23:30:36 +0530

    added test for registered client add

--- src/test/java/com/ecommerce/userservice/RegisteredClientAddTest.java
+++ src/test/java/com/ecommerce/userservice/RegisteredClientAddTest.java
@@ -16,4 +16,29 @@ import java.util.UUID;
 
 @SpringBootTest
 public class RegisteredClientAddTest {
+
+    @Inject
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Inject
+    private RegisteredClientRepository registeredClientRepository;
+
+    @Test
+    public void addRegisteredClientTest() {
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("productservice")
+                .clientSecret(bCryptPasswordEncoder.encode("productservicesecret"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+                .redirectUri("https://oauth.pstmn.io/v1/callback")
+                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        registeredClientRepository.save(oidcClient);
+    }
 }
